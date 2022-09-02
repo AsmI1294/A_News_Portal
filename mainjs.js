4bb13fffd1931897b1a3c08eb55e5fec6accd8f2
ASMIttehad <ittehad.akash@gmail.com>
2022-09-03 00:36:04 +0200

    complete catagoryBar

--- mainjs.js
+++ mainjs.js
@@ -1,12 +1,24 @@
 const url = `https://openapi.programming-hero.com/api/news/categories`;
 fetch(url)
   .then((res) => res.json())
-  .then((data) => console.log(data.data.news_category))
+  .then((data) => categoryBar(data.data.news_category))
   .catch((error) => console.log(error));
 
-let currentCategory = 1;
+let currentCategory = "01";
 
 const categoryBar = (jsonCategory) => {
   let categories = document.getElementById("categories");
-  let categoryNames = jsonCategory.map((x) => {});
+
+  jsonCategory.forEach((x) => {
+    let activeClass = "";
+    if (x.category_id === currentCategory) {
+      activeClass = "active";
+    }
+    let liElement = document.createElement("li");
+    liElement.classList.add("nav-item", "me-2");
+    liElement.innerHTML = `
+    <a class="nav-link ${activeClass}" aria-current="page" href="#"">${x.category_name}</a>
+    `;
+    categories.appendChild(liElement);
+  });
 };
